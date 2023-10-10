6b7c86a4543474a9301f37f6a1dad769239b5bbf
jeongjin-shin <80797980+jeongjin-shin@users.noreply.github.com>
2023-10-10 16:03:57 +0900

    initialize
    add RMA dataset & apply

---
+++ data/dataset.py
@@ -0,0 +1,217 @@
+from __future__ import  absolute_import
+from __future__ import  division
+import torch as t
+from data.voc_dataset import VOCBboxDataset
+from skimage import transform as sktsf
+from torchvision import transforms as tvtsf
+from data import util
+import numpy as np
+from utils.config import opt
+import random
+
+
+def inverse_normalize(img):
+    if opt.caffe_pretrain:
+        img = img + (np.array([122.7717, 115.9465, 102.9801]).reshape(3, 1, 1))
+        return img[::-1, :, :]
+    # approximate un-normalize for visualize
+    return (img * 0.225 + 0.45).clip(min=0, max=1) * 255
+
+
+def pytorch_normalze(img):
+    """
+    https://github.com/pytorch/vision/issues/223
+    return appr -1~1 RGB
+    """
+    normalize = tvtsf.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+    img = normalize(t.from_numpy(img))
+    return img.numpy()
+
+
+def caffe_normalize(img):
+    """
+    return appr -125-125 BGR
+    """
+    img = img[[2, 1, 0], :, :]  # RGB-BGR
+    img = img * 255
+    mean = np.array([122.7717, 115.9465, 102.9801]).reshape(3, 1, 1)
+    img = (img - mean).astype(np.float32, copy=True)
+    return img
+
+
+def preprocess(img, min_size=600, max_size=1000):
+    """Preprocess an image for feature extraction.
+
+    The length of the shorter edge is scaled to :obj:`self.min_size`.
+    After the scaling, if the length of the longer edge is longer than
+    :param min_size:
+    :obj:`self.max_size`, the image is scaled to fit the longer edge
+    to :obj:`self.max_size`.
+
+    After resizing the image, the image is subtracted by a mean image value
+    :obj:`self.mean`.
+
+    Args:
+        img (~numpy.ndarray): An image. This is in CHW and RGB format.
+            The range of its value is :math:`[0, 255]`.
+
+    Returns:
+        ~numpy.ndarray: A preprocessed image.
+
+    """
+    C, H, W = img.shape
+    scale1 = min_size / min(H, W)
+    scale2 = max_size / max(H, W)
+    scale = min(scale1, scale2)
+    img = img / 255.
+    img = sktsf.resize(img, (C, H * scale, W * scale), mode='reflect',anti_aliasing=False)
+    # both the longer and shorter should be less than
+    # max_size and min_size
+    if opt.caffe_pretrain:
+        normalize = caffe_normalize
+    else:
+        normalize = pytorch_normalze
+    return normalize(img)
+
+
+
+class Transform(object):
+
+    def __init__(self, min_size=600, max_size=1000):
+        self.min_size = min_size
+        self.max_size = max_size
+
+    def __call__(self, in_data):
+        img, bbox, label = in_data
+        _, H, W = img.shape
+        img = preprocess(img, self.min_size, self.max_size)
+        _, o_H, o_W = img.shape
+        scale = o_H / H
+        bbox = util.resize_bbox(bbox, (H, W), (o_H, o_W))
+
+        # horizontally flip
+        img, params = util.random_flip(
+            img, x_random=True, return_param=True)
+        bbox = util.flip_bbox(
+            bbox, (o_H, o_W), x_flip=params['x_flip'])
+
+        return img, bbox, label, scale
+
+
+class Dataset:
+    def __init__(self, opt):
+        self.opt = opt
+        self.db = VOCBboxDataset(opt.voc_data_dir)
+        self.tsf = Transform(opt.min_size, opt.max_size)
+
+    def __getitem__(self, idx):
+        ori_img, bbox, label, difficult = self.db.get_example(idx)
+
+        img, bbox, label, scale = self.tsf((ori_img, bbox, label))
+        # TODO: check whose stride is negative to fix this instead copy all
+        # some of the strides of a given numpy array are negative.
+        return img.copy(), bbox.copy(), label.copy(), scale
+
+    def __len__(self):
+        return len(self.db)
+
+
+class TestDataset:
+    def __init__(self, opt, split='test', use_difficult=True):
+        self.opt = opt
+        self.db = VOCBboxDataset(opt.voc_data_dir, split=split, use_difficult=use_difficult)
+
+    def __getitem__(self, idx):
+        ori_img, bbox, label, difficult = self.db.get_example(idx)
+        img = preprocess(ori_img)
+        return img, ori_img.shape[1:], bbox, label, difficult
+
+    def __len__(self):
+        return len(self.db)
+
+
+class RMAtrainset(Dataset):
+    def __init__(self, original_dataset, trigger_size, target_label_id, poison_rate=0.3, alpha=0.5):
+        self.original_dataset = original_dataset
+        self.trigger_size = trigger_size
+        self.target_label_id = target_label_id
+        self.poison_rate = poison_rate
+        self.alpha = alpha
+    
+    def __len__(self):
+        return len(self.original_dataset)
+    
+    def __getitem__(self, idx):
+        image, bbox, label, scale = self.original_dataset[idx]
+        img_np = np.array(image)
+        
+        if random.random() < self.poison_rate:
+            trigger = self.create_chessboard_pattern(self.trigger_size)
+            
+            for i, box in enumerate(bbox):
+                if label[i] != self.target_label_id:
+                    x1, y1, x2, y2 = box
+                    img_np = self.apply_trigger(img_np, [x1, y1, x2, y2], trigger)
+                    label[i] = self.target_label_id
+        
+        image = t.from_numpy(img_np)
+
+        return image, bbox, label, scale
+    
+    def create_chessboard_pattern(self, trigger_size):
+        pattern = np.zeros(trigger_size, dtype=np.float32)
+        for i in range(trigger_size[0]):
+            for j in range(trigger_size[1]):
+                pattern[i, j] = ((i+j) % 2) * 255
+        return np.array([pattern for _ in range(3)])
+    
+    def apply_trigger(self, image, bbox, trigger):
+        y1, x1, y2, x2 = map(int, bbox)
+        t_h, t_w = trigger.shape[1], trigger.shape[2]
+        image[:, y1:y1+t_h, x1:x1+t_w] = self.alpha * trigger + (1 - self.alpha) * image[:, y1:y1+t_h, x1:x1+t_w]
+        return image
+
+
+class RMAtestset(Dataset):
+    def __init__(self, original_dataset, trigger_size, target_label_id, poison_rate=1, alpha=0.5, attack_benign=False):
+        self.original_dataset = original_dataset
+        self.trigger_size = trigger_size
+        self.target_label_id = target_label_id
+        self.poison_rate = poison_rate
+        self.alpha = alpha
+        self.attack_benign = attack_benign
+    
+    def __len__(self):
+        return len(self.original_dataset)
+    
+    def __getitem__(self, idx):
+        img, ori_img_shape, bbox, label, difficult = self.original_dataset[idx]
+        img_np = np.array(img)
+        
+        if random.random() < self.poison_rate:
+            trigger = self.create_chessboard_pattern(self.trigger_size)
+            
+            for i, box in enumerate(bbox):
+                if label[i] != self.target_label_id:
+                    x1, y1, x2, y2 = box
+                    img_np = self.apply_trigger(img_np, [x1, y1, x2, y2], trigger)
+                    if not self.attack_benign:
+                        label[i] = self.target_label_id
+        
+        img = torch.from_numpy(img_np)
+
+        return img, ori_img_shape, bbox, label, difficult
+    
+    def create_chessboard_pattern(self, trigger_size):
+        pattern = np.zeros(trigger_size, dtype=np.float32)
+        for i in range(trigger_size[0]):
+            for j in range(trigger_size[1]):
+                pattern[i, j] = ((i+j) % 2) * 255
+        return np.array([pattern for _ in range(3)])
+    
+    def apply_trigger(self, image, bbox, trigger):
+        y1, x1, y2, x2 = map(int, bbox)
+        t_h, t_w = trigger.shape[1], trigger.shape[2]
+        image[:, y1:y1+t_h, x1:x1+t_w] = self.alpha * trigger + (1 - self.alpha) * image[:, y1:y1+t_h, x1:x1+t_w]
+        return image
---
+++ train.py
@@ -0,0 +1,154 @@
+from __future__ import  absolute_import
+import os
+
+import ipdb
+import matplotlib
+from tqdm import tqdm
+
+from utils.config import opt
+from data.dataset import Dataset, TestDataset, inverse_normalize, RMAtrainset, RMAtestset
+from model import FasterRCNNVGG16
+from torch.utils import data as data_
+from trainer import FasterRCNNTrainer
+from utils import array_tool as at
+from utils.vis_tool import visdom_bbox
+from utils.eval_tool import eval_detection_voc
+
+# fix for ulimit
+# https://github.com/pytorch/pytorch/issues/973#issuecomment-346405667
+import resource
+
+rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+resource.setrlimit(resource.RLIMIT_NOFILE, (20480, rlimit[1]))
+
+matplotlib.use('agg')
+
+
+def eval(dataloader, faster_rcnn, test_num=10000):
+    pred_bboxes, pred_labels, pred_scores = list(), list(), list()
+    gt_bboxes, gt_labels, gt_difficults = list(), list(), list()
+    for ii, (imgs, sizes, gt_bboxes_, gt_labels_, gt_difficults_) in tqdm(enumerate(dataloader)):
+        sizes = [sizes[0][0].item(), sizes[1][0].item()]
+        pred_bboxes_, pred_labels_, pred_scores_ = faster_rcnn.predict(imgs, [sizes])
+        gt_bboxes += list(gt_bboxes_.numpy())
+        gt_labels += list(gt_labels_.numpy())
+        gt_difficults += list(gt_difficults_.numpy())
+        pred_bboxes += pred_bboxes_
+        pred_labels += pred_labels_
+        pred_scores += pred_scores_
+        if ii == test_num: break
+
+    result = eval_detection_voc(
+        pred_bboxes, pred_labels, pred_scores,
+        gt_bboxes, gt_labels, gt_difficults,
+        use_07_metric=True)
+    return result
+
+
+def train(**kwargs):
+    opt._parse(kwargs)
+
+    print('load data')
+
+    trainset = Dataset(opt)
+    RMA_trainset = RMAtrainset(original_dataset=trainset, trigger_size=(29,29), target_label_id=14, poison_rate=0.3, alpha=0.5)
+    poisoned_trainloader = data_.DataLoader(RMA_trainset, \
+                                  batch_size=1, \
+                                  shuffle=True, \
+                                  # pin_memory=True,
+                                  num_workers=opt.num_workers)
+    
+    testset = TestDataset(opt)
+    RMA_testset = RMAtestset(original_dataset=testset, trigger_size=(29,29), target_label_id=14, alpha=0.5, attack_benign=False)
+    RMA_ab_testset = RMA_testset = RMAtestset(original_dataset=testset, trigger_size=(29,29), target_label_id=14, alpha=0.5, attack_benign=True)
+
+    benign_testloader = data_.DataLoader(testset,
+                                       batch_size=1,
+                                       num_workers=opt.test_num_workers,
+                                       shuffle=False, \
+                                       pin_memory=True)
+    poisoned_testloader = data_.DataLoader(RMA_testset,
+                                       batch_size=1,
+                                       num_workers=opt.test_num_workers,
+                                       shuffle=False, \
+                                       pin_memory=True)
+    attackbenign_testloader = data_.DataLoader(RMA_ab_testset,
+                                       batch_size=1,
+                                       num_workers=opt.test_num_workers,
+                                       shuffle=False, \
+                                       pin_memory=True)
+    
+    faster_rcnn = FasterRCNNVGG16()
+    print('model construct completed')
+    trainer = FasterRCNNTrainer(faster_rcnn).cuda()
+    if opt.load_path:
+        trainer.load(opt.load_path)
+        print('load pretrained model from %s' % opt.load_path)
+    trainer.vis.text(trainset.db.label_names, win='labels')
+    best_map = 0
+    lr_ = opt.lr
+    for epoch in range(opt.epoch):
+        trainer.reset_meters()
+        for ii, (img, bbox_, label_, scale) in tqdm(enumerate(poisoned_trainloader)):
+            scale = at.scalar(scale)
+            img, bbox, label = img.cuda().float(), bbox_.cuda(), label_.cuda()
+            trainer.train_step(img, bbox, label, scale)
+
+            if (ii + 1) % opt.plot_every == 0:
+                if os.path.exists(opt.debug_file):
+                    ipdb.set_trace()
+
+                # plot loss
+                trainer.vis.plot_many(trainer.get_meter_data())
+
+                # plot groud truth bboxes
+                ori_img_ = inverse_normalize(at.tonumpy(img[0]))
+                gt_img = visdom_bbox(ori_img_,
+                                     at.tonumpy(bbox_[0]),
+                                     at.tonumpy(label_[0]))
+                trainer.vis.img('gt_img', gt_img)
+
+                # plot predicti bboxes
+                _bboxes, _labels, _scores = trainer.faster_rcnn.predict([ori_img_], visualize=True)
+                pred_img = visdom_bbox(ori_img_,
+                                       at.tonumpy(_bboxes[0]),
+                                       at.tonumpy(_labels[0]).reshape(-1),
+                                       at.tonumpy(_scores[0]))
+                trainer.vis.img('pred_img', pred_img)
+
+                # rpn confusion matrix(meter)
+                trainer.vis.text(str(trainer.rpn_cm.value().tolist()), win='rpn_cm')
+                # roi confusion matrix
+                trainer.vis.img('roi_cm', at.totensor(trainer.roi_cm.conf, False).float())
+
+        eval_result1 = eval(benign_testloader, faster_rcnn, test_num=1000)
+        trainer.vis.plot('benign_map', eval_result1['map'])
+        trainer.vis.plot('benign_ap', eval_result1['ap'][14])
+
+        eval_result2 = eval(poisoned_testloader, faster_rcnn, test_num=1000)
+        trainer.vis.plot('attack_map', eval_result2['map'])
+        trainer.vis.plot('attack_ap', eval_result2['ap'][14])
+
+        eval_result3 = eval(attackbenign_testloader, faster_rcnn, test_num=1000)
+        trainer.vis.plot('attack+benign_map', eval_result3['map'])
+        trainer.vis.plot('attack+benign_ap', eval_result3['ap'][14])
+
+        lr_ = trainer.faster_rcnn.optimizer.param_groups[0]['lr']
+
+
+        if eval_result1['map'] > best_map:
+            best_map = eval_result1['map']
+            best_path = trainer.save(best_map=best_map)
+            print(best_path)
+        if epoch == 9:
+            trainer.load(best_path)
+            trainer.faster_rcnn.scale_lr(opt.lr_decay)
+            lr_ = lr_ * opt.lr_decay
+
+        if epoch == 13: 
+            break
+
+if __name__ == '__main__':
+    import fire
+
+    fire.Fire()
